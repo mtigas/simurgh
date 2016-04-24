9ac57d225244f62da1113a28b9cf9856c12c6974
Mike Tigas <mike@tig.as>
2016-04-24 00:25:25 -0400

    time handling

--- server.go
+++ server.go
@@ -15,15 +15,16 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 package main
 
-import "bytes"
+//import "bytes"
 import "encoding/binary"
 import "net"
 import "fmt"
 import "bufio"
 import "time"
+//import "os"
+//import "strconv"
 
 func main() {
-
   fmt.Println("Launching server...")
 
   // listen on all interfaces
@@ -48,20 +49,22 @@ func main() {
       break
     }
 
-    parsePrevMessage := false
-
-    switch current_message[0] {
-    case 0x31, 0x32, 0x33, 0x34:
-      parsePrevMessage = true
-    }
-
+    // Add to our own buffer (or create it)
     if buffered_message == nil {
       buffered_message = current_message
     } else {
       buffered_message = append(buffered_message, current_message...)
     }
 
-    if !parsePrevMessage {
+    // Are we on a *real* "message start" boundary? Then we're done
+    // with our buffer.
+    parseBuffer := false
+    switch current_message[0] {
+    case 0x31, 0x32, 0x33, 0x34:
+      parseBuffer = true
+    }
+
+    if !parseBuffer {
       continue
     } else {
       message := buffered_message
@@ -72,19 +75,20 @@ func main() {
 
       switch msgType {
         case 0x31:
-          fmt.Print("Type 1 Mode-AC")
+          //fmt.Print("Type 1 Mode-AC")
           msgLen = 10 // 2 + 8 header
         case 0x32:
-          fmt.Print("Type 2 Mode-S short")
+          //fmt.Print("Type 2 Mode-S short")
           msgLen = 15 // 7 + 8 header
         case 0x33:
-          fmt.Print("Type 3 Mode-S long")
+          //fmt.Print("Type 3 Mode-S long")
           msgLen = 22 // 14
         case 0x34:
-          fmt.Print("Status Signal")
+          //fmt.Print("Status Signal")
           msgLen = 10 // ??
         default:
-          msgLen = 8 // shortest possible msg w/header & timetstamp
+          continue
+          //msgLen = 8 // shortest possible msg w/header & timetstamp
       }
 
       // Message wasn't long enough to contain the full header (maybe
@@ -100,77 +104,67 @@ func main() {
       }
       fmt.Print("\n")
 
-      //if message[0] != 0x1a {
-      //  fmt.Print("err")
-      //  break
-      //}
 
-      //timestamp := parseTime(message[2:8])
-      //fmt.Print(timestamp)
-
-      //fmt.Print("\n\n")
+      timestamp := parseTime(message[1:7])
+      fmt.Print(timestamp)
     }
 
   }
 
 }
 
+
 func parseTime(timebytes []byte) time.Time {
-  for i:= 0; i < len(timebytes); i++ {
-    fmt.Printf("%02x", timebytes[i])
-  }
-  iTime := binary.LittleEndian.Uint16(timebytes)
-  fmt.Print("\n")
-  fmt.Println(iTime)
+  // Takes a 6 byte array, which represents a 48bit GPS timestamp
+  // http://wiki.modesbeast.com/Radarcape:Firmware_Versions#The_GPS_timestamp
+  // and parses it into a Time.time
 
+  upper := []byte{
+    timebytes[0] << 2 + timebytes[1] >> 6,
+    timebytes[1] << 2 + timebytes[2] >> 6}
+  lower := []byte{
+    timebytes[2]&0x3F, timebytes[3], timebytes[4], timebytes[5]}
 
-  data := []byte{0x02, 0x18, 0x5f, 0x10, 0x5f, 0x1c, 0x00, 0x00}
-  buf := bytes.NewBuffer(data)
-  var iTimeb int
-  binary.Read(buf, binary.BigEndian, &iTimeb)
-  fmt.Print("\n")
-  fmt.Println(iTimeb)
+  //for i:= 0; i < len(upper); i++ {
+  //  fmt.Printf("%#02x, ", upper[i])
+  //}
+  //fmt.Print("\n")
+  //for i:= 0; i < len(lower); i++ {
+  //  fmt.Printf("%#02x, ", lower[i])
+  //}
+  //fmt.Print("\n")
 
-  return time.Now()
+  daySeconds  := binary.BigEndian.Uint16(upper)
+  nanoSeconds := int(binary.BigEndian.Uint32(lower))
+
+  hr  := int(daySeconds/3600)
+  min := int(daySeconds/60 % 60)
+  sec := int(daySeconds%60)
+
+  //fmt.Print("\n")
+  //fmt.Println(daySeconds)
+  //fmt.Println(nanoSeconds)
+  //fmt.Println(hr)
+  //fmt.Println(min)
+  //fmt.Println(sec)
+
+  localDate := time.Now()
+
+  utcDate := localDate.UTC()
+
+  //var t time.Time
+  //if (utcDate != localDate) && (hr == localDate.Hour()) {
+  //  t = time.Date(
+  //    localDate.Year(), localDate.Month(), localDate.Day(),
+  //    hr, min, sec, nanoSeconds, time.Local)
+  //} else {
+  //  t = time.Date(
+  //    utcDate.Year(), utcDate.Month(), utcDate.Day(),
+  //    hr, min, sec, nanoSeconds, time.UTC)
+  //}
+  //return t
+
+  return time.Date(
+    utcDate.Year(), utcDate.Month(), utcDate.Day(),
+    hr, min, sec, nanoSeconds, time.UTC)
 }
-
-
-//func (b *bufio.Reader) ReadBeastBytes(delim byte) (line [], err error) {
-//	// Use ReadSlice to look for array,
-//	// accumulating full buffers.
-//	var frag []byte
-//	var full [][]byte
-//	var err error
-//	for {
-//		var e error
-//		frag, e = b.ReadSlice(delim)
-//		if e == nil { // got final fragment
-//			break
-//		}
-//		if e != ErrBufferFull { // unexpected error
-//			err = e
-//			break
-//		}
-//
-//		// Make a copy of the buffer.
-//		buf := make([]byte, len(frag))
-//		copy(buf, frag)
-//		full = append(full, buf)
-//	}
-//
-//	// Allocate new buffer to hold the full pieces and the fragment.
-//	n := 0
-//	for i := range full {
-//		n += len(full[i])
-//	}
-//	n += len(frag)
-//
-//	// Copy full pieces and fragment in.
-//	buf := make([]byte, n)
-//	n = 0
-//	for i := range full {
-//		n += copy(buf[n:], full[i])
-//	}
-//	copy(buf[n:], frag)
-//	return buf, err
-//}
